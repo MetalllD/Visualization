270720188d4e3470c502fa8c11a8aec6ef9436df
Mital Kamani <45828315+MetalllD@users.noreply.github.com>
2022-10-31 15:59:00 -0400

    fetched required datapoints

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
   async function fetchPost(setItems) {
     const response = await fetch(
-        "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=go3zpdsGv6uMVAS1L1dSK3X02Aq7i2xQDZzdtl9c"
+        "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=1&api_key=go3zpdsGv6uMVAS1L1dSK3X02Aq7i2xQDZzdtl9c"
       );
 
     const food = await response.json();
     setItems(food);
-    console.log(food);
+    console.log(food.foods[0].foodNutrients[0].nutrientName);
   };
 
 
@@ -16,10 +16,13 @@ function App() {
 
   const [items, setItems] = useState([]);
 
-
   useEffect(() => {
     fetchPost(setItems);
   }, []);
+
+  /*console.log('data outside hook', items.map((m) => <span>${m.foods[0].foodNutrients[0].nutrientName}</span>));*/
+
+  /*const inject = items ? items.map((it, i) => <li key={i}>{it.foods[0].foodNutrients[i].nutrientName}</li>) : null;*/
 
   return (
     <div className="App">
