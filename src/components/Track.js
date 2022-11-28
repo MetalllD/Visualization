549cae3f9b8c57a78c7947a3b30701979b6f6160
Mital Kamani <45828315+MetalllD@users.noreply.github.com>
2022-11-28 15:12:09 -0500

    Integrated P5.js

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,11 +1,61 @@
 import React from "react";
+import { useEffect, useState } from 'react';
+import Sketch from "react-p5";
+import Search from './SearchBar';
+import { setup, draw } from "react-p5";
 
+async function fetchPost(setItems,x) {
+    const response = await fetch(
+        "https://api.nal.usda.gov/fdc/v1/foods/search?query="+ x +"&pageSize=1&api_key=go3zpdsGv6uMVAS1L1dSK3X02Aq7i2xQDZzdtl9c"
+      );
+
+    const food = await response.json();
+    setItems(food.foods[0].foodNutrients);
+}
+ 
 export default function Track() {
 
+    const [items, setItems] = useState([]);
 
-    return(
-        <div className="head">
-            Here you can calculate total calories of the food you consume daily!!
-        </div>
-    );
+    const { search } = window.location;
+    const query = new URLSearchParams(search).get('s');
+    const [searchInput, setSearchInput] = useState(query || 'Cheese');
+  
+    useEffect(() => {
+      fetchPost(setItems,searchInput);
+    }, [searchInput]);
+
+    console.log(items);
+
+    const dummy= items.filter((item) => item.nutrientName==="Energy").map((item)=> item.nutrientNumber);
+    const energy = dummy[0];
+    console.log(energy);
+
+
+//P5 visualization
+
+let x = 350;
+let y = 200;
+ 
+setup = (p5, parent) => {
+p5.createCanvas(700, 500).parent(parent);
+p5.background(0);
+p5.fill(140, 191, 248, 60);
+;}
+
+draw = p5 => {
+p5.circle(x, y, energy);
 }
+ 
+ 
+return (
+    <div>
+    <Search 
+          searchQuery={searchInput}
+          setSearchQuery={setSearchInput}
+        /> 
+    <Sketch setup={setup} draw={draw} />
+    </div>
+)
+}
+ 
